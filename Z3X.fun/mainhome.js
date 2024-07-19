9bbf7c67967ec20887b65e6b7efb324616e09f6a
Xgx78cv789CGcb897Ffs <142930265+iaiskncjlasdurffaxkklfhiaf@users.noreply.github.com>
2024-07-19 21:20:54 +0300

    Delete Z3X.fun directory

--- Z3X.fun/mainhome.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('scroll-down-button').addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetElement = document.getElementById('second-layer');
-        const targetOffsetTop = targetElement.getBoundingClientRect().top + window.pageYOffset;
-        window.scrollTo({ top: targetOffsetTop, behavior: 'smooth' });
-    });
-});
